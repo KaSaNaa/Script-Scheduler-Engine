61dfa5b2ea1e0b89ec0e70c171e043016548ff65
KaSaNaa <pasindudeshitha123@gmail.com>
2025-01-12 00:24:25 +0530

    Add function declarations and task struct to header files

--- include/scheduler.h
+++ include/scheduler.h
@@ -1,6 +1,13 @@
 #ifndef SCHEDULER_H
 #define SCHEDULER_H
 
+typedef struct {
+    int priority;
+    char *script_name;
+    // A simple way to prioritize tasks.
+    // * Other fields like status, execution time, etc. can be added here.
+} Task;
+
 void initialize_scheduler(void);
 void run_scheduler(void);
 
