1370403cc2a3a6603334653838ea36969ec60af7
KaSaNaa <pasindudeshitha123@gmail.com>
2025-01-12 00:24:35 +0530

    Add script execution to scheduler and improve initialization logging

--- src/scheduler.c
+++ src/scheduler.c
@@ -1,10 +1,14 @@
 #include "scheduler.h"
+#include "func.h"
 #include <stdio.h>
 
 void initialize_scheduler(void) {
+    // ! Initialize the scheduler (setup necessary data structures, etc.)
     printf("Scheduler Initialized.\n");
 }
 
 void run_scheduler(void) {
     printf("Scheduler Running.\n");
+    printf("Executing script: task1.py\n");
+    execute_script("scripts/test1.py");
 }
