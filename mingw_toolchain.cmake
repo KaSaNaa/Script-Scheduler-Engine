eda8575f29adb96de02e8025192995476297f236
KaSaNaa <pasindudeshitha123@gmail.com>
2025-01-12 12:57:16 +0530

    Add MinGW toolchain configuration and fix CMakeLists newline issue

---
+++ mingw_toolchain.cmake
@@ -0,0 +1,5 @@
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_C_COMPILER C:/msys64/mingw64/bin/gcc.exe)
+set(CMAKE_CXX_COMPILER C:/msys64/mingw64/bin/g++.exe)
+set(CMAKE_MAKE_PROGRAM C:/msys64/mingw64/bin/mingw32-make.exe)
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
