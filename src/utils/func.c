07e5355895d6bc37c66015d3687f4022fb9e31ab
KaSaNaa <pasindudeshitha123@gmail.com>
2025-01-12 12:57:28 +0530

    Enhance execute_script function with current directory logging and script existence check

--- src/utils/func.c
+++ src/utils/func.c
@@ -1,9 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
-void execute_script(const char *script) {// this will execute a bash or python script depending on the system
+void execute_script(const char *script) { // this will execute a bash or python script depending on the system
+    char cwd[256];
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        printf("Current working dir: %s\n", cwd);
+    } else {
+        perror("getcwd() error");
+    }
+
+    if (access(script, F_OK) != -1){
+        printf("Script file exists: %s\n", script);
+    } else {
+        printf("Script file doesnt not exist: %s\n", script);
+        return;
+    }
 
     char command[256];
     snprintf(command, sizeof(command), "python %s", script); // Adjust based on script type
+    printf("Executing command: %s\n", command);
     system(command);
 }
