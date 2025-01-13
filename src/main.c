370fd6372012ac29c5c0664043ce39c5298c06e0
KaSaNaa <pasindudeshitha123@gmail.com>
2025-01-13 15:41:38 +0530

    Add tasks to main function for script execution in scheduler

--- src/main.c
+++ src/main.c
@@ -2,6 +2,13 @@
 
 int main(void) {
     initialize_scheduler();
+    
+    Task task1 = {1, "scripts/test1.py"};
+    Task task2 = {2, "scripts/test2.sh"};
+
+    add_task(task1);
+    add_task(task2);
+
     run_scheduler();
     return 0;
 }
