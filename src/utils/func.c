22c2b6ccd4bf4d71b1eb0b6ca72acaf2d1951b78
KaSaNaa <pasindudeshitha123@gmail.com>
2025-01-13 15:41:33 +0530

    Remove debug print statements from execute_script function for cleaner output

--- src/utils/func.c
+++ src/utils/func.c
@@ -5,15 +5,18 @@
 void execute_script(const char *script) { // this will execute a bash or python script depending on the system
     char cwd[256];
     if (getcwd(cwd, sizeof(cwd)) != NULL) {
-        printf("Current working dir: %s\n", cwd);
+        // printf("Current working dir: %s\n", cwd);
+        printf("\n");
     } else {
         perror("getcwd() error");
     }
 
     if (access(script, F_OK) != -1){
-        printf("Script file exists: %s\n", script);
+        // printf("Script file exists: %s\n", script);
+        printf("\n");
     } else {
-        printf("Script file doesnt not exist: %s\n", script);
+        // printf("Script file doesnt not exist: %s\n", script);
+        printf("\n");
         return;
     }
 
